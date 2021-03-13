f85ba8afec2bdc92413bab7158cb471d260b0f93
roundapv <80544196+roundapv@users.noreply.github.com>
2021-03-14 01:30:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
-
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DOGE:DRsqCZY668yhy5cuoxxmXciLjp2UymzyUV -p c=DOGE
 
 done
+
